555e08fa9875be489f5411c871fd056de525221c
Nicolas Olmos <yohannicolas2003@gmail.com>
2022-04-24 12:38:56 -0500

    Make invoice a spring component

--- src/main/java/com/rd/co/class2di/domain/Invoice.java
+++ src/main/java/com/rd/co/class2di/domain/Invoice.java
@@ -2,9 +2,11 @@ package com.rd.co.class2di.domain;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class Invoice {
     @Value("${invoice.description}")
     private String description;
